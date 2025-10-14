5042389702edc9670ab92e1af20f6087562cd29d
WIFI0000 <rachel.1749216@gmail.com>
2025-10-14 16:52:49 +0800

    Site setting error

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://WIFI0000.github.io",
+	site: "https://WIFI0000.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
