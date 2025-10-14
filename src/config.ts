2fbd471a4648ac07d0bf76e0209b4d499dfae665
WIFI0000 <rachel.1749216@gmail.com>
2025-10-15 00:07:02 +0800

    Change banner image

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/banner/d7.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/d1.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -30,10 +30,6 @@ export const siteConfig: SiteConfig = {
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
-		{
-			src: "/favicon/icon.png",
-			sizes: "32x32",
-		},
 		// Leave this array empty to use the default favicon
 		// {
 		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
